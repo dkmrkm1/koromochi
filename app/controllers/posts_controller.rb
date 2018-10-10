ff6e30c7b17927125e25d2bb0ac778d909ca7811
Duuun <daimura10@gmail.com>
2018-10-10 23:44:45 +0900

    降順に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    # @posts = Post.all
-    @posts = Post.page(params[:page]).per(5)
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
