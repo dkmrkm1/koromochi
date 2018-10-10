5617bf34bd4676dab6abe5806eb37ed17ac2bba8
Duuun <daimura10@gmail.com>
2018-10-10 23:58:21 +0900

    編集画面へのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
   post "posts/create" => "posts#create"
+  get "post/:id/edit" => "post#edit"
 
   get '/' => "home#top"
   get 'about' => "home#about"
