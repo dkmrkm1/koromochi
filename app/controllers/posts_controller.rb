7cdfa29e0558d07d01046e2ff24e85118a9b85e2
Duuun <daimura10@gmail.com>
2018-10-14 01:22:12 +0900

    新規作成時、削除時のメッセージを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(content: params[:content])
     if @post.save
+      flash[:notice] = "投稿を作成しました"
       redirect_to("/posts/index")
     else
       render("posts/new")
@@ -43,7 +44,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
-
+    flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
 end
