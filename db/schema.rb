2567852d232c38a29d4534ac507fa13d6d177f18
Duuun <daimura10@gmail.com>
2018-10-19 02:18:31 +0900

    Userモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_013340) do
+ActiveRecord::Schema.define(version: 2018_10_18_171559) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
