909f98b177d216461ea8f56c364a15147555b122
Duuun <daimura10@gmail.com>
2018-10-26 02:05:02 +0900

    :thumbsup: ログイン処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,12 @@ class UsersController < ApplicationController
 
   # ログイン処理
   def login
-    
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      render("users/login_form")
+    end
   end
 end
