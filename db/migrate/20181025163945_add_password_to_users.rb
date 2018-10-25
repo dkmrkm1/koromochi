981925881f39e177b10da404a2e89d52ab74f6bc
Duuun <daimura10@gmail.com>
2018-10-26 01:48:35 +0900

    Userテーブルにpasswordカラム、validationを追加

--- db/migrate/20181025163945_add_password_to_users.rb
+++ db/migrate/20181025163945_add_password_to_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_columns :users, :password, :string
+    add_column :users, :password, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_171559) do
+ActiveRecord::Schema.define(version: 2018_10_25_163945) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_10_18_171559) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
