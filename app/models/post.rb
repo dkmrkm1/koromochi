fdacc6a002c82254e3e1a8410ef8ab02ef5b097c
Duuun <daimura10@gmail.com>
2018-10-30 08:16:44 +0900

    postsとuserの紐付けを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   # validates :検証するカラム名、{検証する内容}
   validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id, {presence: true}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_163945) do
+ActiveRecord::Schema.define(version: 2018_10_29_231330) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
