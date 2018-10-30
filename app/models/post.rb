a004004ab50aa289c01ee827fefadfa5cfcef70b
Duuun <daimura10@gmail.com>
2018-10-30 09:36:46 +0900

    added title to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   # validates :検証するカラム名、{検証する内容}
+  validates :title, {presence: true}
   validates :content, {presence: true, length: {maximum: 140}}
   validates :user_id, {presence: true}
 end
